f22eca234234021687327225e725a53aced4e34b
hwassman <hwassman@de.ibm.com>
2020-11-16 10:41:59 +0100

    add .circleci/config.yml

---
+++ tests/test_params.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('./source/')
+import zimonGrafanaIntf
+import os
+
+def test_case01():
+    result = os.system("python ./source/zimonGrafanaIntf.py")
+    assert result == 0
+
+def test_case02():
+    result = os.system("python ./source/zimonGrafanaIntf.py --port 4242")
+    assert result == 0
+
+def test_case03():
+    result = os.system("python ./source/zimonGrafanaIntf.py -c 10")
+    assert result == 0
+
+def test_case04():
+    result = os.system("python ./source/zimonGrafanaIntf.py --port 8443")
+    assert result == 0
+
+def test_case05():
+    result = os.system('python ./source/zimonGrafanaIntf.py --port 8443 --keyPath "C:\\tmp\\"')
+    assert result == 0
+
+def test_case06():
+    result = os.system('python ./source/zimonGrafanaIntf.py -a 2')
+    assert result > 0
+
+
+
