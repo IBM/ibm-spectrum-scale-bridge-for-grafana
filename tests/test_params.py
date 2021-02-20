069f7278d9edbdc439fb2524452b52229324f5b7
hwassman <hwassman@de.ibm.com>
2021-02-20 23:55:11 +0100

    add missing blanks

--- tests/test_params.py
+++ tests/test_params.py
@@ -5,21 +5,26 @@ def test_case01():
     result = parse_defaults_from_config_file()
     assert isinstance(result, dict)
 
+
 def test_case02():
     result = parse_defaults_from_config_file()
     assert len(result.keys()) > 0
 
+
 def test_case03():
     result = parse_defaults_from_config_file()
     elements = list(result.keys())
-    mandatoryItems = ['port','serverport']
+    mandatoryItems = ['port', 'serverport']
     assert all(item in elements for item in mandatoryItems)
+
 
 def test_case04():
     result = parse_defaults_from_config_file()
     value = int(result['port'])
     assert value == 4242
 
+
 def test_case05():
     result = parse_defaults_from_config_file()
     assert int(result['port']) == 4242 and int(result['serverport']) == 9084
+
