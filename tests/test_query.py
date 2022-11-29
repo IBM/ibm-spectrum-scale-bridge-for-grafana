394d2af3f54530a7d1f53952ac7323987ad9b57f
hwassman <hwassman@de.ibm.com>
2022-11-29 15:24:09 +0100

    extend query tests
    Signed-off-by: hwassman <hwassman@de.ibm.com>

--- tests/test_query.py
+++ tests/test_query.py
@@ -4,9 +4,10 @@ from nose2.tools.decorators import with_setup
 
 
 def my_setup():
-    global metrics, metrics1
+    global metrics, metrics1, capSensors
     metrics = ['cpu_user']
     metrics1 = ['gpfs_fs_inode_used']
+    capSensors = ['GPFSDiskCap','GPFSPoolCap', 'GPFSInodeCap']
 
 @with_setup(my_setup)
 def test_case01():
@@ -19,3 +20,20 @@ def test_case01():
 def test_case02():
     query = Query(metrics1)
     assert "-ar" in str(query)
+    query.addMetric('cpu_user')
+    assert "-ar" in str(query)
+
+@with_setup(my_setup)
+def test_case03():
+    for sensor in capSensors:
+        query = Query()
+        query.sensor = sensor
+        assert "-ar" in str(query)
+    query = Query()
+    query.sensor = 'CPU'
+    assert "-ar" not in str(query)
+    assert "group" in str(query)
+    query = Query(includeDiskData=True)
+    query.sensor = 'GPFSDiskCap'
+    assert "-ar" in str(query)
+    
