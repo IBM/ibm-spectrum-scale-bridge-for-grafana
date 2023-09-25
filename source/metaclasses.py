98504c6c8dc689c411e5ba5c7763619526213db9
hwassman <hwassman@de.ibm.com>
2023-09-25 16:21:21 +0200

    fix missed blanks

--- source/metaclasses.py
+++ source/metaclasses.py
@@ -20,9 +20,11 @@ Created on Sep 22, 2023
 @author: HWASSMAN
 '''
 
+
 class Singleton(type):
     """metaclass to make a class a singleton"""
     _instances = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
