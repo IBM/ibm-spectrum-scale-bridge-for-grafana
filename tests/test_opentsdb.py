d55a19cfb81c5413e222c3c9bbf2c6f1c5e32caf
hwassman <hwassman@de.ibm.com>
2025-05-03 21:34:42 +0200

    wrong indentation
    Signed-off-by: hwassman <hwassman@de.ibm.com>

--- tests/test_opentsdb.py
+++ tests/test_opentsdb.py
@@ -123,9 +123,9 @@ def query_raw_data_setup():
                                 {'filter': 'scale-11', 'groupBy': False,
                                  'tagk': 'node', 'type': 'pm_filter'
                                  }],
-                           'metric': 'cpu_system', 'index': 0,
-                           'shouldComputeRate': True, 'isCounter': False
-                           }
+                            'metric': 'cpu_system', 'index': 0,
+                            'shouldComputeRate': True, 'isCounter': False
+                            }
             }
     jreq2 = {'start': 1739214930519, 'end': 1739215230519, 'arrays': True,
              'inputQuery': {'aggregator': 'noop', 'downsample': '1m-avg',
@@ -135,7 +135,7 @@ def query_raw_data_setup():
                                  }],
                             'metric': 'cpu_system', 'index': 0,
                             'shouldComputeRate': True, 'isCounter': True
-                           }
+                            }
             }
     jreq3 = {'start': 1746277483949, 'end': None,
              'inputQuery': {'aggregator': 'noop', 'downsampleAggregator': 'avg',
@@ -152,7 +152,7 @@ def query_raw_data_setup():
                             'disableDownsampling': True,
                             'explicitTags': False, 'index': 0
                             }
-             }
+            }
 
 
 @with_setup(my_setup)
