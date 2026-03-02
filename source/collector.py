9d2fb5a095590fa795df3e731564e58f4946ee68
hwassman <hwassman@de.ibm.com>
2026-03-02 20:30:56 +0100

    refine null values messages
    Signed-off-by: hwassman <hwassman@de.ibm.com>

--- source/collector.py
+++ source/collector.py
@@ -103,7 +103,7 @@ class TimeSeries(object):
                 {timestmp: self.dps[timestmp]} for timestmp in timestamps if
                 self.dps[timestmp] is not None), {})
             if len(self.dps) == 0:
-                logger.details(MSG['NullValues'].format(self.metricname))
+                logger.debug(MSG['NullValues'].format(self.metricname, '|'.join(self.tags.values())))
 
 
 class MetricTimeSeries(object):
